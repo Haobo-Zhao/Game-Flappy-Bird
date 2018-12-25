98c71d44a5a3b9409efcece714b82d08a509f9d8
Haobo-Zhao <haobo4608@gmail.com>
2018-12-25 12:51:54 -0500

    implemented jumping for bird

--- scene/gaming/bird.js
+++ scene/gaming/bird.js
@@ -1,9 +1,31 @@
 class Bird extends Animation {
     constructor(game) {
         super(game, 'yellowbird')
+        this.vy = 0
+        this.ay = 0.2
     }
 
     static new(game) {
         return new this(game)
+    }
+
+    update() {
+        super.update()
+        this.y += this.vy
+        this.vy += this.ay
+        // 限制 y方向的速度
+        if (this.vy > 10) {
+            this.vy = 10
+        }
+        if (this.y + this.h > 700) {
+            // 最后那个 2 是微调的效果
+            // magic, don't touch
+            this.y = 700 - this.h + 2
+        }
+    }
+
+    jump() {
+        this.y -= 20
+        this.vy = 0
     }
 }
--- scene/title/scene_title.js
+++ scene/title/scene_title.js
@@ -2,9 +2,25 @@ class Scene_title extends Scene {
     constructor(game) {
         super(game)
         let ground = Ground.new(game)
+        this.ground = ground
         this.addElement(ground)
-
+        
         let bird = Bird.new(game)
+        this.bird = bird
         this.addElement(bird)
+
+        this.setupInputs()
+    }
+
+    setupInputs() {
+        let self = this
+        // this.game.registerAction(' ', function(){
+        //     self.bird.jump()
+        // })
+        window.addEventListener('keydown', function(event){
+            if (event.key === ' ') {
+                self.bird.jump()
+            }
+        })
     }
 }
