4bfe9e31bbf3cc9528d3fec74c271ad7cc851744
Haobo-Zhao <haobo4608@gmail.com>
2018-12-29 02:34:51 -0500

    added utilities, and rewrite bindEvents

--- game/config.js
+++ game/config.js
@@ -34,9 +34,12 @@ const controlTemplate = function (key, item) {
     // date-key是用来确定，这个控制组件要修改的是 config 的哪一个对象
     let t = `
         <div class="container-control-${key}">
-            <input id="id-${key}-input"
+            <input 
+                id="id-${key}-input"
+                class="joe-slider-control"
                 type="range" 
-                data-key="${key}" value="${item.value}" 
+                value="${item.value}" 
+                data-item="config.${key}" 
                 min="${item.min || 1}" max="${item.max || 300}"
             >
             <span class="description-control">${item.value}</span>
@@ -55,24 +58,28 @@ const generateAllControls = function () {
     }
 }
 
+// const bindEvents = function () {
+//     let keys = Object.keys(config)
+//     for (let k of keys) {
+//         let ele = e(`#id-${k}-input`)
+//         let parent = ele.closest(`.container-control-${k}`)
+//         ele.addEventListener('input', () => {
+//             // 在这里，动态调整全局配置的值
+//             eval(`${ele.dataset.item}.value = ${ele.value}`)
+//             parent.querySelector('.description-control').innerText = ele.value
+//         })
+//     }
+// }
+
 const bindEvents = function () {
-    let keys = Object.keys(config)
-    for (let k of keys) {
-        let ele = e(`#id-${k}-input`)
-        let parent = ele.closest(`.container-control-${k}`)
-        ele.addEventListener('input', () => {
-            parent.querySelector('.description-control').innerText = ele.value
-            // 在这里，动态调整全局配置的值
-
-            // 这个写法很容易忽略掉把字符串转换成数字
-            // config[k].value = ele.value
-
-            // 下面的写法有个大坑，很容易掉进去，
-            // 就是忘记在方括号里面加上引号，
-            // 不加引号，方括号里面的值就变成一个变量，而不是一个字符串，就找不到了
-            eval(`config['${k}'].value = ${ele.value}`)
-        })
-    }
+    bindAll('.joe-slider-control', 'input', function (event) {
+        // 拿到变动的 element
+        let target = event.target
+        // 更新全局配置
+        eval(`${target.dataset.item}.value = ${target.value}`)
+        // 更新这个 item 显示的值
+        target.closest('div').querySelector('.description-control').innerHTML = target.value
+    })
 }
 
 const setConfiguration = function () {
--- game/utils.js
+++ game/utils.js
@@ -5,6 +5,20 @@ const e = function(selector) {
     return document.querySelector(selector)
 }
 
+// 多重影分身,es = elements
+const es = selector => document.querySelectorAll(selector)
+
+const bindAll = function(selector, eventType, callback) {
+    var elements = es(selector)
+    for (let e of elements) {
+        // 后面用一个函数表达式，把 callback 包起来，这样更加灵活，想做什么操作都可以在里面做
+        // 而不需要严格适配传进来的 callback
+        e.addEventListener(eventType, function(event) {
+            callback(event)
+        })
+    }
+}
+
 const imageFromPath = function(path) {
     var image = new Image()
     image.src = path
